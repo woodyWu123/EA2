0aeb0626866af83c9a714bc228281b63b033fbe0
Frankie2005 <frankiechoi2905@gmail.com>
2025-07-09 23:36:09 +0800

    1.12

--- 3006_EA/viorng.java
+++ 3006_EA/viorng.java
@@ -1,11 +1,18 @@
 const images = [
-    'https://www.youtube.com/embed/FPJ2T26ydlk?autoplay=1&mute=1&controls=0&loop=1&playlist=FPJ2T26ydlk',
-    'https://www.youtube.com/embed/JmSC9xahhyA?autoplay=1&mute=1&controls=0&loop=1&playlist=JmSC9xahhyA',
-    'https://www.youtube.com/embed/4xxbhYRvW5Y?autoplay=1&mute=1&controls=0&loop=1&playlist=4xxbhYRvW5Y',
-    'https://www.youtube.com/embed/kXj0o0gIbho?autoplay=1&mute=1&controls=0&loop=1&playlist=kXj0o0gIbho',
-    'https://www.youtube.com/embed/LRvzUgyJucA?autoplay=1&mute=1&controls=0&loop=1&playlist=LRvzUgyJucA'
+            'https://www.youtube.com/embed/FPJ2T26ydlk?autoplay=1&mute=1&controls=0&loop=1&playlist=FPJ2T26ydlk',
+            'https://www.youtube.com/embed/JmSC9xahhyA?autoplay=1&mute=1&controls=0&loop=1&playlist=JmSC9xahhyA',
+            'https://www.youtube.com/embed/4xxbhYRvW5Y?autoplay=1&mute=1&controls=0&loop=1&playlist=4xxbhYRvW5Y',
+            'https://www.youtube.com/embed/kXj0o0gIbho?autoplay=1&mute=1&controls=0&loop=1&playlist=kXj0o0gIbho',
+            'https://www.youtube.com/embed/LRvzUgyJucA?autoplay=1&mute=1&controls=0&loop=1&playlist=LRvzUgyJucA'
 ];
-
+        const images1 = [
+            'https://www.youtube.com/watch?v=FPJ2T26ydlk',
+            'https://www.youtube.com/watch?v=JmSC9xahhyA',
+            'https://www.youtube.com/watch?v=4xxbhYRvW5Y',
+            'https://www.youtube.com/watch?v=kXj0o0gIbho',
+            'https://www.youtube.com/watch?v=LRvzUgyJucA'
+        ];
 // 隨機選擇一張圖片
 const randomIndex = Math.floor(Math.random() * images.length);
 document.getElementById('video-background').src = images[randomIndex];
+document.getElementById('video-Link').href = images1[randomIndex];
